0ef1fa76c9a78f384df528b72750048374157bea
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-05 16:13:13 +0000

    Exercise Tracker - 3more test cases pending

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // index.js
 // where your node app starts
+require('dotenv').config();
 
 // init project
 var express = require('express');
 var app = express();
+
+// Accessing dns module
+const dns = require('dns');
+//Accessing MongoDB
+let mongoose = require('mongoose');
+//Body Parser
+let bodyParser = require('body-parser');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -13,6 +21,9 @@ let methodRun = false;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use('/api/shorturl',bodyParser.urlencoded({extended: false})); 
+app.use('/api/users',bodyParser.urlencoded({extended: false})); 
+app.use('/api/users/:_id/exercises',bodyParser.urlencoded({extended: false}));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,8 +35,6 @@ app.get("/api/hello", function (req, res) {https://ak-freeCodehttps://ak-freeCod
   res.json({greeting: 'hello API'});
 });
 
-
-
 //Request Header Parser Microservice
 app.get('/api/whoami', (req, res) => {
   console.log("GET > /api/whoami");
@@ -34,14 +43,134 @@ app.get('/api/whoami', (req, res) => {
             "software":req.headers['user-agent']});
 });
 
-// Accessing dns module
-const dns = require('dns');
-let bodyParser = require('body-parser');
-app.use('/api/shorturl',bodyParser.urlencoded({extended: false})); 
+//Connect MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let redirectUrl = ['https://forum.freecodecamp.org/'];
+const userSchema = new mongoose.Schema({
+    username : {
+      type: String,
+      required: true
+    },
+    count : {
+      type: Number
+    },
+    log : [
+      {
+        description : String,
+        duration : Number,
+        date: String
+      }
+    ]
+});
+
+let user = mongoose.model("User", userSchema);
+
+//Exercise Tracker
+app.post('/api/users', (req, res) => {
+  console.log("POST > /api/users");
+  console.log(req.body);
+  let userName = req.body.username;
+  user.find({username:userName},function(err, data) {
+    console.log(data);
+     if (data.length == 0)
+      {
+       let userDetail = new user({username:userName, count:0 });
+        userDetail.save(function(err, savedData) {
+          if (err && err.code == "ENOTFOUND")
+          {
+            console.error(err);
+            return;
+          }
+          console.log(savedData.username + "  " + savedData._id);
+          res.json({username:savedData.username,_id:savedData._id});
+        });
+      }
+      else
+      {
+        console.log(data[0].username + "  " + data[0]._id);
+        res.json({username:data[0].username,_id:data[0]._id});
+      }
+    
+  });
+});
+
+app.get('/api/users', (req, res) => {
+  console.log("GET > /api/users");
+  user.find()
+    .select({count: 0,log:0,__v:0})
+    .exec(function(err, data) {
+      if (err) return console.error(err);
+      res.json(data);
+    });
+});
+
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  let userId = req.params._id;
+  console.log("POST > /api/users/"+userId+"/exercises");
+  
+  let exerciseDetail = {
+    description: req.body.description,
+    duration: req.body.duration == '' ? 0 : Number(req.body.duration),
+    date: req.body.date == '' ? new Date().toDateString() : new Date(req.body.date).toDateString()
+  };
+  console.log(exerciseDetail);
+  user.findById(userId,
+                function(err, data) {
+                  data.count++;
+                  data.log.push(exerciseDetail);
+
+                  data.save((err, updatedUser) => {
+                    console.log(updatedUser);
+                    console.log(updatedUser.username);
+                    if (err) return console.error(err);
+                    res.json({
+                      username: updatedUser.username,
+                      description: exerciseDetail.description,
+                      duration: exerciseDetail.duration,
+                      date: exerciseDetail.date,
+                      _id: userId
+                    });
+                  });
+  });
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  let userId = req.params._id;
+  console.log(req.method + " > "+ req.path);
+  console.log(req.params);
+  console.log(req.query);
+
+  let filterCondition = {};
+  let limitCondition = {}
+  if(req.query.from != undefined)
+  {
+    filterCondition.$gte = new Date(req.query.from).toDateString();
+  }
+  if(req.query.to != undefined)
+  {
+    filterCondition.$lte = new Date(req.query.to).toDateString();
+  }
+
+  if(req.query.limit != undefined)
+  {
+    limitCondition.limit = Number(req.query.limit);
+  }
+
+  console.log(filterCondition);
+  console.log(limitCondition);
+user.find({_id: userId, date: filterCondition},limitCondition).select().exec(
+  function(err, data) {
+    if (err) return console.error(err);
+    console.log(data);
+    res.json(data[0]);
+  });
+});
+
+
 
 //URL Shortener Microservice
+let redirectUrl = ['https://forum.freecodecamp.org/'];
 app.post('/api/shorturl', (req, res) => {
   console.log("POST > /api/shorturl");
   let urlLink = req.body.url;
@@ -98,7 +227,7 @@ app.get('/api/:date?', (req, res) => {
   var dateOutput;
 
   //Added due to conflict with other projects
-  if(dateInput == "shorturl")
+  if(dateInput == "shorturl" || dateInput == "users")
   {
     return;
   }
