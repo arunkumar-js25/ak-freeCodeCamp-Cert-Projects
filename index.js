a4f1f4e8fad07fef0947f14be292cf6808cedf53
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-05 11:15:07 +0000

    URL Shortener Microservice - changes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+let methodRun = false;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,9 +20,11 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", function (req, res) {https://ak-freeCodehttps://ak-freeCodeCamp-Cert-Projects.arunkumar-js25.repl.coCamp-Cert-Projects.arunkumar-js25.repl.co
   res.json({greeting: 'hello API'});
 });
+
+
 
 //Request Header Parser Microservice
 app.get('/api/whoami', (req, res) => {
@@ -29,6 +32,59 @@ app.get('/api/whoami', (req, res) => {
   res.json({"ipaddress":req.headers['x-forwarded-for'],
             "language":req.headers['accept-language'],
             "software":req.headers['user-agent']});
+});
+
+// Accessing dns module
+const dns = require('dns');
+let bodyParser = require('body-parser');
+app.use('/api/shorturl',bodyParser.urlencoded({extended: false})); 
+
+let redirectUrl = ['https://forum.freecodecamp.org/'];
+
+//URL Shortener Microservice
+app.post('/api/shorturl', (req, res) => {
+  console.log("POST > /api/shorturl");
+  let urlLink = req.body.url;
+  let indexPos=0;
+  let errFlag = false;
+  //Check whether the url is valid or not
+  dns.lookup( urlLink, function(err, address, family){
+    console.log(err);
+    if(err){
+      errFlag = true;
+    }
+  });
+
+  if (errFlag) {
+        res.json({ error: 'invalid url' });
+  }
+  else
+  {
+    indexPos = redirectUrl.indexOf(urlLink);
+    console.log(indexPos);
+    if( indexPos == -1){
+      redirectUrl.push(urlLink);
+      indexPos = redirectUrl.length-1;
+    }
+  res.json({ original_url : urlLink, short_url : indexPos});
+  }
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  
+  let inputID = req.params.id;
+  console.log("GET > /api/shorturl/"+inputID);
+  console.log(inputID);
+  console.log(redirectUrl);
+  if(inputID != undefined && /^\d+$/.test(inputID) && inputID < redirectUrl.length)
+  {
+    console.log(redirectUrl[inputID]);
+    res.redirect(redirectUrl[inputID]);
+  }
+  else
+  {
+    res.json({ error: 'invalid ID' });
+  }
 });
 
 //Timestamp Microservice 
